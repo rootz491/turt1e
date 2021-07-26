0e584a01e40e95edd55714561460d8e3f81b33d6
rootz491 <experimentyt1@gmail.com>
2021-07-26 16:44:51 +0530

    init

---
+++ app.js
@@ -0,0 +1,9 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(cors);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
